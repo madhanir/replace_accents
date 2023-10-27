88d73cd40a7f35065d18502e4e27deb3ba410822
Rahul Madhani <madhani.rahul@gmail.com>
2023-10-27 18:31:38 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    LONG_DESCRIPTION = f.read()
+
+setup(
+    name="replace_accents",
+    version="0.0.3",
+    description="This package helps to replace accented characters with their corresponding non-accented ascii characters",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    author="Rahul Madhani",
+    author_email="madhani.rahul@gmail.com",
+    packages=find_packages(),
+)
