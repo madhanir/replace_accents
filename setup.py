d3630e01f0778ed1b72f68ecb8360d16396597b1
Rahul Madhani <madhani.rahul@gmail.com>
2024-05-05 21:33:46 +0530

    Added source repository for the package

--- setup.py
+++ setup.py
@@ -5,11 +5,24 @@ with open("README.md", "r") as f:
 
 setup(
     name="replace_accents",
-    version="0.0.4",
+    version="0.0.5",
     description="This package helps to replace accented characters with their corresponding non-accented ascii characters",
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     author="Rahul Madhani",
     author_email="madhani.rahul@gmail.com",
     packages=find_packages(),
+    url="https://medium.com/@rahul.madhani/replace-accented-characters-in-python-pyspark-and-spark-sql-28844b6c783e",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries",
+    ],
+    project_urls={
+        "Documentation": "https://medium.com/@rahul.madhani/replace-accented-characters-in-python-pyspark-and-spark-sql-28844b6c783e",
+        "Source": "https://github.com/madhanir/replace_accents",
+    },
 )
